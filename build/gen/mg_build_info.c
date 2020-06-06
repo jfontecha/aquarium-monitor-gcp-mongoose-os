6d5577ad3b47db67f6824c854cecd0bbd212fced
jfontecha <jesus.fontecha1@gmail.com>
2020-06-06 10:12:26 +0200

    remove wrong build folder

--- build/gen/mg_build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *mg_build_id = "20200605-175910/2.17.0-gc31a745";
-const char *mg_build_timestamp = "2020-06-05T17:59:10Z";
-const char *mg_build_version = "2.17.0";
